ad32505eb13c7102c55148fa629d335bdd712c21
Luis Ayala <lfayala9@gmail.com>
2024-06-09 01:44:24 +0200

    finished

--- get_next_line.c
+++ get_next_line.c
@@ -18,7 +18,7 @@ char	*read_file(int fd, char *buffer)
 	int		bytes;
 
 	if (read(fd, 0, 0) < 0)
-		return (free(buffer), NULL);
+		return(free(buffer), NULL);
 	bytes = 1;
 	tmp_buff = (char *)malloc(BUFFER_SIZE + 1);
 	if (!tmp_buff)
@@ -27,17 +27,18 @@ char	*read_file(int fd, char *buffer)
 	{
 		bytes = read(fd, tmp_buff, BUFFER_SIZE);
 		if (bytes == -1)
+		{
 			return (free(tmp_buff), NULL);
+		}
 		tmp_buff[bytes] = '\0';
 		buffer = ft_strjoin(buffer, tmp_buff);
 		if (buffer == 0)
-			return (free(buffer), NULL);
+			return (free(tmp_buff), NULL);
 	}
-	free (tmp_buff);
-	return (buffer);
+	return (free(tmp_buff), buffer);
 }
 
-char	*get_line(char *buffer)
+char	*getting_line(char *buffer)
 {
 	char	*line;
 	int		line_break;
@@ -84,8 +85,7 @@ char	*store_buffer(char *buffer)
 	while (buffer[line_break])
 		tmp_buf[i++] = buffer[line_break++];
 	tmp_buf[i] = '\0';
-	free(buffer);
-	return (tmp_buf);
+	return (free(buffer), tmp_buf);
 }
 
 char	*get_next_line(int fd)
@@ -93,27 +93,30 @@ char	*get_next_line(int fd)
 	static char	*buffer;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE < 1)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = read_file(fd, buffer);
 	if (!buffer)
 		return (NULL);
-	line = get_line(buffer);
+	line = getting_line(buffer);
 	buffer = store_buffer(buffer);
 	return (line);
 }
 
-// int	main()
+// int	main(int ac, char **av)
 // {
 // 	int fd;
 // 	char *line;
-// 	fd = open("text.txt", O_RDONLY);
-//     while ((line = get_next_line(fd)) != NULL)
-//     {
-//         printf("%s", line);
-//         free(line);
-//     }
 
+// 	if (ac > 1)
+// 	{
+// 		fd = open(av[1], O_RDONLY);
+//     	while ((line = get_next_line(fd)) != NULL)
+//     	{
+//         	printf("line: -> %s", line);
+//         	free(line);
+//     	}
+// 	}
 // 	close(fd);
 // 	return 0;
 // }
